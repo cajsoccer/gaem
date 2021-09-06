ab0566b87be68021ca0dacb0f1efc2c0d4afe954
Carter <cajsoccer@live.com>
2021-09-05 23:36:11 -0400

    added keymovement & fixed double moving ticks

--- src/comps/SnakeBoard.js
+++ src/comps/SnakeBoard.js
@@ -6,13 +6,36 @@ for (let i = 0; i < 20; i++)
 for (let i = 0; i < 20; i++)
 	for (let j = 0; j < 20; j++)
 		rows[i][j] = 0;
+let direction = "down";
 const getRandPos = () => [Math.floor(Math.random() * 20), Math.floor(Math.random() * 20)];
+window.addEventListener('keydown', e => 
+{
+	switch(e.key)
+	{
+		case "ArrowDown":
+			if (direction !== "up")
+				direction = "down";
+			break;
+		case "ArrowUp":
+			if (direction !== "down")
+				direction = "up";
+			break;
+		case "ArrowLeft":
+			if (direction !== "right")
+				direction = "left";
+			break;
+		case "ArrowRight":
+			if (direction !== "left")
+				direction = "right";
+			break;
+	}
+});
 const checkGameOver = snake =>
 {
 	if (snake[0][0] < 0 || snake[0][1] << 0)
 		return true;
 	for (let i = 1; i < snake.length; i++)
-		if (snake[0][0] == snake[i][0] && snake[0][1] == snake[i][1])
+		if (snake[0][0] === snake[i][0] && snake[0][1] === snake[i][1])
 			return true;
 	return false;
 }
@@ -100,6 +123,7 @@ const moveSnake = (board, snake, fruitPos, direction) =>
 			}
 			break;
 	}
+	return {board, snake, fruitPos};
 }
 const boxType = num => 
 {
@@ -113,7 +137,7 @@ const boxType = num =>
 			return "blue-box";
 	}
 }
-let playerPos = [[10,10], [10,11], [10,12], [10, 13]]; //[11,10], [10,10], [10,11], [10, 12];
+let playerPos = [[10,10], [10,11], [10,12], [10, 13]];
 let fruitPos = [16, 10];
 rows[fruitPos[0]][fruitPos[1]] = 1;
 playerPos.forEach(p => rows[p[0]][p[1]] = 2);
@@ -123,19 +147,20 @@ export default class Board extends React.Component
     constructor(props) 
 	{
         super(props);
-        this.state = { board: rows, snake: playerPos };
+        this.state = { board: rows, snake: playerPos, fruit: fruitPos };
     }
     componentDidMount() 
 	{
-        setInterval(() => 
+        this.ticker = setInterval(() => 
 		{
-            this.setState(state => 
-			{
-                moveSnake(state.board, state.snake, fruitPos, "down");
-                return { board: state.board, snake: state.snake };
-            });
-        }, 1000);
+            let newState = moveSnake(this.state.board, this.state.snake, this.state.fruit, direction);
+			this.setState(newState);
+        }, 100);
     }
+	componentWillUnmount() 
+	{
+		clearInterval(this.ticker);
+	}
 	render() 
 	{
 		return (
